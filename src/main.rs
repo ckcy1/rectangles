f1a298c715f211905aaac530f1c7fcac647ca748
andy <ckcy1@qq.com>
2022-10-09 06:24:51 +0800

    使用结构梯来重构代码

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
+struct Rectangle{
+    width:u32,
+    height:u32,
+}
+
 fn main() {
     // let width1 =30;
     // let height1 =50;
-    let rect1 = (30,50);
+    // let rect1 = (30,50);
+let rect1=Rectangle{width:30,height:50};
     println!(
         "The area of the rectangle is {} square pixels.",
         // area(width1,height1)
-         area(rect1)
+        //  area(rect1)
+        area(&rect1)
     );
    // println!("Hello, world!");
-    fn area(dimensions:(u32,u32))->u32{
-       dimensions.0 * dimensions.1
+   //  fn area(dimensions:(u32,u32))->u32{
+   //     dimensions.0 * dimensions.1
+// }
+fn area(rectangle:&Rectangle)->u32{
+       rectangle.height * rectangle.width
    }
 }
